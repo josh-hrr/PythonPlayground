895eb273ac574c3a4d245d6c80d164a4536c6278
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-09-20 19:44:12 -0600

    final project version 0.01

---
+++ Platzi/final_project/hotel_management/customers.py
@@ -0,0 +1,20 @@
+class Customer:
+    def __init__():
+        pass
+
+class CustomerManagement:
+    counter = 0
+    def __init__(self):
+        self.customers = {
+            
+        }
+
+    def add_customer(self, customer):
+        """Agrega un nuevo cliente al sistema."""
+        counter += 1
+        self.consumers[counter] = customer
+
+    def get_customer(self, customer_id):
+       """Obtiene la información de un cliente por ID."""
+               
+       pass
