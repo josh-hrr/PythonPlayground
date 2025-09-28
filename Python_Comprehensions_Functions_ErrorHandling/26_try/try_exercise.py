5be5945245181af0292a64cbd40c57e15d4403d7
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-09-28 13:35:17 -0600

    try except exercise

---
+++ Python_Comprehensions_Functions_ErrorHandling/26_try/try_exercise.py
@@ -0,0 +1,13 @@
+def my_divide(a, b): 
+   try:
+      result = a / b
+      return result
+   except ZeroDivisionError as e:
+      return "No se puede dividir por 0"
+   
+    
+response = my_divide(10, 2)
+print(response) # 5
+
+response = my_divide(10, 0)
+print(response) # No se puede dividir por 0
