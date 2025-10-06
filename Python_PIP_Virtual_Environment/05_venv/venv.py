8b75b2b2f104ec732e4ddfb9689512f71f6b4091
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2025-10-05 20:50:37 -0600

    practice pip concept, pip freeze, virtual environment setup using python3 -m venv my_env, WSL concept

---
+++ Python_PIP_Virtual_Environment/05_venv/venv.py
@@ -0,0 +1,15 @@
+'''
+
+python3 -m venv my_env
+Creates a virtual environment named 'my_env' in the current directory.
+
+isolates dependencies for different projects.
+
+need to be installed only once per project.
+command: sudo apt install -y python3-venv
+command: source my_env/bin/activate
+command: deactivate
+
+'''
+
+print('This is a virtual environment example.')
